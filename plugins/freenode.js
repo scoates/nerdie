04b70cde872192ed6596b452b1dbbfc3cb0f5df3
Sean Coates <sean@seancoates.com>
2011-08-01 15:42:13 -0400

    add freenode plugin for NickServ

---
+++ plugins/freenode.js
@@ -0,0 +1,30 @@
+var request = require('request');
+
+var NerdieInterface = require('nerdie_interface.js');
+
+var nerdie;
+
+var isConnected = false;
+
+function Freenode(parentNerdie) {
+	nerdie = parentNerdie;
+	this.pluginInterface = new NerdieInterface(parentNerdie, this);
+}
+
+Freenode.prototype.init = function () {
+	this.pluginInterface.registerPattern(
+		'.',
+		function (msg) {
+			// only do this once
+			if (isConnected) {
+				return;
+			}
+			isConnected = true;
+			if (nerdie.config.plugins.freenode && nerdie.config.plugins.freenode.password) {
+				nerdie.bot.say('NickServ', 'id ' + nerdie.config.plugins.freenode.password);
+			}
+		}
+	);
+};
+
+module.exports = Freenode;
